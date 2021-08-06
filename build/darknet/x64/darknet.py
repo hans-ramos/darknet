1668d7337daf56b5b2cfcffba1d5e96ff304b959
hans-ramos <jester_ramos@dlsu.edu.ph>
2021-08-06 17:16:17 +0800

    Update darknet.py

--- build/darknet/x64/darknet.py
+++ build/darknet/x64/darknet.py
@@ -135,7 +135,7 @@ def draw_boxes(detections, image, colors):
         left, top, right, bottom = bbox2points(bbox)
         cv2.rectangle(image, (left, top), (right, bottom), colors[label], 1)
         cv2.putText(image, "{} [{:.2f}]".format(label, float(confidence)),
-                    (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (right, bottom - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     colors[label], 2)
     return image
 
